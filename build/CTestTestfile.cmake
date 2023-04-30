31315bc81d59c65e945a78713062d9936ca9e309
Sahil-Programme <sahilyadav127@gmail.com>
2023-05-01 09:48:50 +1000

    CMake list configured - learning

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/dev/c++/OMRON_control
+# Build directory: D:/dev/c++/OMRON_control/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
